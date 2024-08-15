963cd40afe29a3a9bf48d81473366f0c5392e87a
We4not <gor.gulkanyan.list.com@gmail.com>
2024-08-15 21:39:05 +0300

    updated installmodules.sh

--- installmodules.sh
+++ installmodules.sh
@@ -1,5 +1,7 @@
-pip3 install colorama
-pip3 install psutil
-pip3 install logging
-pip3 install configparser
-pip3 install datetime
+mkdir installmodules-log
+
+pip3 install colorama >> log.txt
+pip3 install psutil >> log.txt
+pip3 install logging >> log.txt
+pip3 install configparser >> log.txt
+pip3 install datetime >> log.txt
