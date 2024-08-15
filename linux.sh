84a12a8f3f4c40883b3dfbeffedf598e5c9e41fb
We4not <gor.gulkanyan.list.com@gmail.com>
2024-08-15 21:50:59 +0300

    updated linux.sh

--- linux.sh
+++ linux.sh
@@ -4,14 +4,8 @@ cd build
 pyinstaller --onefile ../main.py --icon ../icon.png --name EyeTerminal
 rm -r build
 rm -r EyeTerminal.spec
-pyinstaller --onefile ../crashlog.py --icon ../icon.png --name CrashLog
 rm -r build
-rm -r CrashLog.spec
 ren dist builded
 cd builded
 mkdir log
-mkdir plugin
 touch config.cfg
-touch plugin.cfg
-clear
-echo Please don't forget copy folder plugin, folder log, and files config.cfg, plugin.cfg
