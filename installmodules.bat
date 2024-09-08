e522b163030c8efc8fb2d555dfde667e7bb383ac
We4not <gor.gulkanyan.list.com@gmail.com>
2024-09-08 17:44:47 +0300

    Deleted .bat and .sh

--- installmodules.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-pip install colorama
-pip install psutil
-pip install logging
-pip install configparser
-pip install datetime
--- installmodules.sh
+++
@@ -1,7 +0,0 @@
-mkdir installmodules-log
-
-pip3 install colorama >> installmodules-log/log.txt
-pip3 install psutil >> installmodules-log/log.txt
-pip3 install logging >> installmodules-log/log.txt
-pip3 install configparser >> installmodules-log/log.txt
-pip3 install datetime >> installmodules-log/log.txt
