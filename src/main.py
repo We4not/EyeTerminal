4c765ecbde31c5af78bacf845d365f24d080a05c
We4not <gor.gulkanyan.list.com@gmail.com>
2025-02-05 07:50:27 +0300

    updated file

--- src/main.py
+++ src/main.py
@@ -1,26 +1,35 @@
 import climage
+import colorama
 import sys
 from sys import platform
 from colorama import Fore
 from scripts.user import User
 from scripts.command import Command
+from scripts.config import Config
+
+colorama.init()
 
 class EyeTerminal:
     def __init__(self):
         self.user = User()
-        self.command = Command()
-        if platform == "linux" or platform == "linux2":
-            self.output = climage.convert('../icon.png') # DON'T FORGET CHANGE PATH BEFORE BUILD APPLICATION!!!!
+        self.command = Command(self.user)
+
+        self.config = Config()
+        
+        if self.config.GetPictureOnStart_Value():
+            if platform == "win32" or platform == "linux2":
+                self.output = climage.convert('../icon.png') # DON'T FORGET CHANGE PATH BEFORE BUILD APPLICATION!!!!
 
     def main(self):
-        if platform == "linux" or platform == "linux2":
-            print(self.output)
+        if self.config.GetPictureOnStart_Value():
+            if platform == "win32" or platform == "linux2":
+                print(self.output)
 
-        print(Fore.GREEN + "Welcome to EyeTerminal!" + Fore.RESET)
+        print(f"{Fore.GREEN}Welcome to {self.config.GetTitle_Value()}{Fore.RESET}")
 
         while True:
             self.user.HandleWhile()
-            self.command.HandleUserInput(self.user.input_text)
+            self.command.HandleUserInput()
 
 
 if __name__ == '__main__':
