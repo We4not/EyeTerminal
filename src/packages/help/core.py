2b1abbd33dc834ab96d87b7a1af4e76284e729cd
We4not <gor.gulkanyan.list.com@gmail.com>
2025-02-08 00:34:02 +0300

    added new package

---
+++ src/packages/help/core.py
@@ -0,0 +1,5 @@
+def main(args):
+    print("clear - clear from screen\n"
+          "exit - exit from program\n"
+          "echo [TEXT] - prints the line\n"
+          "version - show current version\n")
